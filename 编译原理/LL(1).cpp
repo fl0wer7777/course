26a18ead210c8ed09dffabe91934b255eaf3f776
fl0wer7777 <3057142482@qq.com>
2022-06-05 22:41:00 +0800

    编译原理

---
+++ 编译原理/LL(1).cpp
@@ -0,0 +1,219 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+void printStack(stack<char> st, string s) {
+		char c;
+		string ss;
+		stack<char> temp;
+		while (!st.empty()) {
+			c = st.top();
+			st.pop();
+			temp.push(c);
+			ss += c;
+		}
+		while (!temp.empty()) {
+			c = temp.top();
+			temp.pop();
+			st.push(c);
+		}
+		reverse(ss.begin(), ss.end());
+		cout << ss << "\t" << s << endl;
+}
+
+int main() {
+	string input;
+	stack<char> st;
+	unordered_map<char, unordered_set<char>> FIRST;
+	unordered_map<char, unordered_set<char>> FOLLOW;
+	unordered_map<char, unordered_map<char, string>> M;
+
+	unordered_set<char> FIRST0 = {};
+	unordered_set<char> FOLLOW0 = { '#' };
+	unordered_set<char> FOLLOW1 = {};
+	unordered_map<char, unordered_map<char, string>> t1;
+	unordered_map<char, string> M0 = {};
+	FIRST['F'] = FIRST0;
+	FIRST['S'] = FIRST0;
+	FIRST['T'] = FIRST0;
+	FIRST['G'] = FIRST0;
+	FIRST['E'] = FIRST0;
+	FOLLOW['F'] = FOLLOW1;
+	FOLLOW['S'] = FOLLOW1;
+	FOLLOW['T'] = FOLLOW1;
+	FOLLOW['G'] = FOLLOW1;
+	FOLLOW['E'] = FOLLOW0;
+	M['F'] = M0;
+	M['S'] = M0;
+	M['T'] = M0;
+	M['G'] = M0;
+	M['E'] = M0;
+
+	vector<vector<string>> grammar = { {"F", "(E)", "i"}, {"S", "e", "*FS", "/FS"}, {"T", "FS"}, {"G", "e", "+TG", "-TG"}, {"E", "TG"} };
+	for (int i = 0; i < grammar.size(); i++) {
+		vector<string> array = grammar[i];
+		char left = array[0][0];
+		for (int j = 1; j < array.size(); j++) {
+			string s = array[j];
+			if (s[0] >= 'A' && s[0] <= 'Z') {
+				for (int k = 0; (k < s.size()) && (s[k] >= 'A' && s[k] <= 'Z'); k++) {
+					for (unordered_set<char>::iterator it = FIRST[s[k]].begin(); it != FIRST[s[k]].end(); ++it) {
+						FIRST[left].insert(*it);
+					}
+					bool flag = 0;
+					for (int m = 0; m < s.size(); m++) {
+						if (s[m] == 'e') {
+							flag = 1;
+						}
+					}
+					if (!flag) {
+						break;
+					}
+					if (k == s.size() - 1) {
+						FIRST[left].insert('e');
+					}
+				}
+			}
+			else {
+				FIRST[left].insert(s[0]);
+			}
+		}
+	}
+	
+	grammar = { {"F", "(E)", "i"}, {"E", "TG"}, {"G", "e", "+TG", "-TG"}, {"S", "e", "*FS", "/FS"}, {"T", "FS"} };
+	for (int i = 0; i < grammar.size(); i++) {
+		vector<string> array = grammar[i];
+		char left = array[0][0];
+		for (int j = 1; j < array.size(); j++) {
+			string s = array[j];
+			for (int k = 0; k < s.size(); k++) {
+				char c = s[k];
+				if (c >= 'A' && c <= 'Z') {
+					if (k == s.size() - 1) {
+						for (unordered_set<char>::iterator it = FOLLOW[left].begin(); it != FOLLOW[left].end(); ++it) {
+							FOLLOW[c].insert(*it);
+						}
+						break;
+					}
+				}
+				unordered_set<char> temp = {};
+				for (int m = k + 1; m < s.size(); m++) {
+					if (s[m] >= 'A' && s[m] <= 'Z') {
+						for (unordered_set<char>::iterator it = FIRST[s[m]].begin(); it != FIRST[s[m]].end(); ++it) {
+							temp.insert(*it);
+						}
+					}
+					else {
+						temp.insert(s[m]);
+						break;
+					}
+					if (FIRST[s[m]].count('e') == 0) {
+						break;
+					}
+					if (m == s.size() - 1) {
+						for (unordered_set<char>::iterator it = FOLLOW[left].begin(); it != FOLLOW[left].end(); ++it) {
+							FOLLOW[c].insert(*it);
+						}
+					}
+				}
+				temp.erase('e');
+				for (unordered_set<char>::iterator it = temp.begin(); it != temp.end(); ++it) {
+					FOLLOW[c].insert(*it);
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < grammar.size(); i++) {
+		vector<string> array = grammar[i];
+		char left = array[0][0];
+		for (int j = 1; j < array.size(); j++) {
+			string s = array[j];
+			if (s[0] >= 'A' && s[0] <= 'Z') {
+				unordered_set<char> temp = {};
+				for (int k = 0; k < s.size(); k++) {
+					for (unordered_set<char>::iterator it = FIRST[s[k]].begin(); it != FIRST[s[k]].end(); ++it) {
+						temp.insert(*it);
+					}
+					if (FIRST[s[k]].count('e') == 0) {
+						temp.erase('e');
+						break;
+					}
+				}
+				for (unordered_set<char>::iterator it = temp.begin(); it != temp.end(); ++it) {
+					if (*it != 'e') {
+						M[left][*it] = s;
+					}
+				}
+				for (unordered_set<char>::iterator it = temp.begin(); it != temp.end(); ++it) {
+					if (*it == 'e') {
+						for (unordered_set<char>::iterator it0 = FOLLOW[left].begin(); it0 != FOLLOW[left].end(); ++it0) {
+							M[left][*it0] = "e";
+						}
+						break;
+					}
+				}
+			}
+			else if (s[0] == 'e') {
+				for (unordered_set<char>::iterator it = FOLLOW[left].begin(); it != FOLLOW[left].end(); ++it) {
+						M[left][*it] = "e";
+				}
+			}
+			else {
+				M[left][s[0]] = s;
+			}
+		}
+	}
+
+
+	input = "i+i*i#";
+
+	st.push('#');
+	st.push('E');
+	
+	printStack(st, input);
+
+	char x = st.top();
+	st.pop();
+	char a = input[0];
+
+	while (true) {
+		if (x == '#' && a == '#') {
+			cout << "Success!" << endl;
+			break;
+		}
+		else if (x != a) {
+			string s;
+			if (M[x].empty()) {
+				s = "";
+				cout << "Error!" << endl;
+				break;
+			}
+			else {
+				s = M[x][a];
+				if (s != "e") {
+					reverse(s.begin(), s.end());
+					for (int i = 0; i < s.size(); i++) {
+						st.push(s[i]);
+					}
+				}
+				printStack(st, input);
+				x = st.top();
+				st.pop();
+			}
+		}
+		else {
+			input.erase(0, 1);
+			a = input[0];
+			printStack(st, input);
+			x = st.top();
+			st.pop();
+		}
+	}
+}
---
+++ 编译原理/lex.cpp
@@ -0,0 +1,226 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+vector<string> key = { "do", "end", "for", "if", "printf", "scanf", "then", "while" };
+vector<string> delimiter = { ",", ";", "(", ")", "{", "}" };
+vector<string> arithmeticOperation = { "+", "-", "*", "/" };
+vector<string> relationalOperation = { "<", "<=", "=", ">", ">=", "<>" };
+
+bool isKey(string s);
+bool isLetter(char c);
+bool isDigit(char c);
+bool isDelimiter(string s);
+bool isArithmeticOperation(string s);
+bool isRelationalOperation(string s);
+void lex(string file);
+
+int main() {
+	string file = "C://Users//dell//Desktop//1.txt";
+	lex(file);
+
+	return 0;
+}
+
+bool isKey(string s) {
+	for (int i = 0; i < key.size(); i++) {
+		if (key[i] == s) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool isLetter(char c) {
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))) {
+		return true;
+	}
+	return false;
+}
+
+bool isDigit(char c) {
+	if (c >= '0' && c <= '9') {
+		return true;
+	}
+	return false;
+}
+
+bool isDelimiter(string s) {
+	for (int i = 0; i < delimiter.size(); i++) {
+		if (delimiter[i] == s) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool isArithmeticOperation(string s) {
+	for (int i = 0; i < arithmeticOperation.size(); i++) {
+		if (arithmeticOperation[i] == s) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool isRelationalOperation(string s) {
+	for (int i = 0; i < relationalOperation.size(); i++) {
+		if (relationalOperation[i] == s) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void lex(string file) {
+	ifstream input;
+	input.open(file, ios::in);
+	if (!input.is_open()) {
+		cout << "Error!" << endl;
+		return;
+	}
+
+	string s = "";
+	char c;
+	int line = 1, row = 0;
+	while ((input.get(c))) {
+		s = "";
+		if ((c == ' ') || (c == '\t')) {}
+		else if (c == '\n') {
+			line++;
+			row = 0;
+		}
+		else if (isLetter(c)) {
+			row++;
+			while (isLetter(c) || isDigit(c) || c == '_') {
+				s += c;
+				input.get(c);
+			}
+			input.seekg(-1, ios::cur);
+			if (isKey(s)) {
+				cout << s << "\t(" << 1 << "," << s << ")" << "\t关键字" << "\t(" << line << "," << row << ")" << endl;
+			}
+			else {
+				cout << s << "\t(" << 6 << "," << s << ")" << "\t标识符" << "\t(" << line << "," << row << ")" << endl;
+			}
+		}
+		else if (isDigit(c)) {
+			row++;
+			while (isDigit(c)) {
+				s += c;
+				input.get(c);
+			}
+			if (isLetter(c)) {
+				while (isLetter(c)) {
+					s += c;
+					input.get(c);
+				}
+				input.seekg(-1, ios::cur);
+				cout << s << "\t(" << 0 << "," << s << ")" << "\tError" << "\t(" << line << "," << row << ")" << endl;
+				continue;
+			}
+			input.seekg(-1, ios::cur);
+			cout << s << "\t(" << 5 << "," << s << ")" << "\t常数" << "\t(" << line << "," << row << ")" << endl;
+		}
+		else {
+			s = "";
+			s += c;
+			if (isArithmeticOperation(s)) {
+				row++;
+				s = "";
+				if (c == '+') {
+					s += c;
+					input.get(c);
+					if (c == '+') {
+						s += c;
+						cout << s << "\t(" << 3 << "," << s << ")" << "\t算术运算符" << "\t(" << line << "," << row << ")" << endl;
+					}
+					else {
+						input.seekg(-1, ios::cur);
+						s = "+";
+						cout << s << "\t(" << 3 << "," << s << ")" << "\t算术运算符" << "\t(" << line << "," << row << ")" << endl;
+					}
+				}
+				else if (c == '-') {
+					s += c;
+					input.get(c);
+					if (c == '-') {
+						s += c;
+						cout << s << "\t(" << 3 << "," << s << ")" << "\t算术运算符" << "\t(" << line << "," << row << ")" << endl;
+					}
+					else {
+						input.seekg(-1, ios::cur);
+						s = "+";
+						cout << s << "\t(" << 3 << "," << s << ")" << "\t算术运算符" << "\t(" << line << "," << row << ")" << endl;
+					}
+				}
+				else {
+					cout << s << "\t(" << 3 << "," << s << ")" << "\t算术运算符" << "\t(" << line << "," << row << ")" << endl;
+				}
+			}
+			else if (isDelimiter(s)) {
+				row++;
+				cout << s << "\t(" << 2 << "," << s << ")" << "\t分界符" << "\t(" << line << "," << row << ")" << endl;
+			}
+			else if (isRelationalOperation(s)) {
+				switch (c) {
+					row++;
+				case '=': {
+					row++;
+					input.get(c);
+					s += c;
+					if (isRelationalOperation(s)) {
+						cout << s << "\t(" << 4 << "," << s << ")" << "\t关系运算符" << "\t(" << line << "," << row << ")" << endl;
+					}
+					else {
+						s.pop_back();
+						cout << s << "\t(" << 4 << "," << s << ")" << "\t关系运算符" << "\t(" << line << "," << row << ")" << endl;
+						input.seekg(-1, ios::cur);
+					}
+				}
+					break;
+				case '>': {
+					row++;
+					input.get(c);
+					s += c;
+					if (isRelationalOperation(s)) {
+						cout << s << "\t(" << 4 << "," << s << ")" << "\t关系运算符" << "\t(" << line << "," << row << ")" << endl;
+					}
+					else {
+						s.pop_back();
+						cout << s << "\t(" << 4 << "," << s << ")" << "\t关系运算符" << "\t(" << line << "," << row << ")" << endl;
+						input.seekg(-1, ios::cur);
+					}
+				}
+					break;
+				case '<': {
+					row++;
+					input.get(c);
+					s += c;
+					if (isRelationalOperation(s)) {
+						cout << s << "\t(" << 4 << "," << s << ")" << "\t关系运算符" << "\t(" << line << "," << row << ")" << endl;
+					}
+					else {
+						s.pop_back();
+						cout << s << "\t(" << 4 << "," << s << ")" << "\t关系运算符" << "\t(" << line << "," << row << ")" << endl;
+						input.seekg(-1, ios::cur);
+					}
+				}
+					break;
+				default: {
+					row++;
+					cout << c << "\t(" << 0 << "," << s << ")" << "\tError" << "\t(" << line << "," << row << ")" << endl;
+				}
+					break;
+				}
+			}
+			else {
+				row++;
+				cout << c << "\t(" << 0 << "," << s << ")" << "\tError" << "\t(" << line << "," << row << ")" << endl;
+			}
+		}
+	}
+	input.close();
+}
